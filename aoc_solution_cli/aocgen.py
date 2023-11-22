3b8aa7476ce4441ed2fc0f0f4493f595e62f0eba
taitep <arvid@h.se>
2023-11-22 17:29:42 +0100

    Change name of main function in aocgen from generate to aocgen

--- aoc_solution_cli/aocgen.py
+++ aoc_solution_cli/aocgen.py
@@ -4,7 +4,7 @@ from recent_problem_saver import get_recent, day_to_module
 @click.command
 @click.option('--day', type=int)
 @click.option('--year', type=int)
-def generate(day, year):
+def aocgen(day, year):
     day, year = get_recent(day, year)
     file = f"{day_to_module(day, year)}.py"
     with open(file, 'w') as f:
@@ -17,4 +17,5 @@ def solution_b(data):
 ''')
 
 if __name__ == '__main__':
-    generate()
+    aocgen()
+
