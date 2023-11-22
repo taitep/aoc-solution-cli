bab08bb8e4818afeda36207e480c2226fccf9722
taitep <arvid@h.se>
2023-11-22 16:35:37 +0100

    Improve solution_pair

--- aoc_solution_cli.py
+++ aoc_solution_cli.py
@@ -15,8 +15,16 @@ def solution_pair(solution_a: Callable[[list[str]], int], solution_b: Callable[[
     
     @solution_cli.command
     @click.option('--submit/--no-submit', 'submit_attempt', default=False, help='Submit the answer')
-    def b(submit_attempt):
-        answer = solution_b(get_data(day=day, year=year))
-        if submit_attempt: submit(answer, day=day, year=year, part='b')
+    @click.option('--data', default='', help='If provided, uses this data instead of fetching from servers')
+    @click.option('--show-output/--hide-output', default=True, help='Print result to stdout')
+    def b(submit_attempt, data, show_output):
+        answer = solution_b((data or get_data(day=day, year=year)).strip().splitlines())
+        if submit_attempt:
+            if not data: submit(answer, day=day, year=year, part='b')
+            else:
+                print("Can not upload answer based on custom data.")
+                exit(1)
+        if show_output: print(f'Result: {answer}')
+        return answer
     
     return solution_cli
