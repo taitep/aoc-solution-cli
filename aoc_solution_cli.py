a5eabc65e245e3a27bdd02a56181b6be6a1efe1a
taitep <arvid@h.se>
2023-11-22 16:36:23 +0100

    Copy changes on part b to part a

--- aoc_solution_cli.py
+++ aoc_solution_cli.py
@@ -9,9 +9,17 @@ def solution_pair(solution_a: Callable[[list[str]], int], solution_b: Callable[[
 
     @solution_cli.command
     @click.option('--submit/--no-submit', 'submit_attempt', default=False, help='Submit the answer')
-    def a(submit_attempt):
-        answer = solution_a(get_data(day=day, year=year))
-        if submit_attempt: submit(answer, day=day, year=year, part='a')
+    @click.option('--data', default='', help='If provided, uses this data instead of fetching from servers')
+    @click.option('--show-output/--hide-output', default=True, help='Print result to stdout')
+    def a(submit_attempt, data, show_output):
+        answer = solution_a((data or get_data(day=day, year=year)).strip().splitlines())
+        if submit_attempt:
+            if not data: submit(answer, day=day, year=year, part='a')
+            else:
+                print("Can not upload answer based on custom data.")
+                exit(1)
+        if show_output: print(f'Result: {answer}')
+        return answer
     
     @solution_cli.command
     @click.option('--submit/--no-submit', 'submit_attempt', default=False, help='Submit the answer')
